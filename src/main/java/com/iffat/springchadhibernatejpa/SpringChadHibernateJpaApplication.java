59adfd53232db93963f8a85e386250c380f9e63e
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-24 14:58:17 +0700

    01. Starter Project

---
+++ src/main/java/com/iffat/springchadhibernatejpa/SpringChadHibernateJpaApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springchadhibernatejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChadHibernateJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChadHibernateJpaApplication.class, args);
+    }
+
+}
