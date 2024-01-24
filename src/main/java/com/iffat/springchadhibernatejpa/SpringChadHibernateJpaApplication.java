11b634f525b208e60e0aa65a819b10391bdb3848
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-25 05:28:21 +0700

    06. One To One Mapping - Main App

--- src/main/java/com/iffat/springchadhibernatejpa/SpringChadHibernateJpaApplication.java
+++ src/main/java/com/iffat/springchadhibernatejpa/SpringChadHibernateJpaApplication.java
@@ -1,5 +1,8 @@
 package com.iffat.springchadhibernatejpa;
 
+import com.iffat.springchadhibernatejpa.dao.AppDAO;
+import com.iffat.springchadhibernatejpa.entity.Instructor;
+import com.iffat.springchadhibernatejpa.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +16,16 @@ public class SpringChadHibernateJpaApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner() {
+    public CommandLineRunner commandLineRunner(AppDAO appDAO) {
         return runner -> {
-            System.out.println("Hello world");
+            createInstructor(appDAO);
         };
+    }
+
+    private void createInstructor(AppDAO appDAO) {
+        Instructor instructor = new Instructor("Muhamad", "Iffat", "Iffat@gmail.com");
+        InstructorDetail instructorDetail = new InstructorDetail("http://www.youtube/muhamad", "football");
+        instructor.setInstructorDetail(instructorDetail);
+        appDAO.save(instructor);
     }
 }
