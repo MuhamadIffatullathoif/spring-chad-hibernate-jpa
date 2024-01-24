57096c68781b67da1e46cfc13c13438a5e587f0b
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-24 15:11:47 +0700

    02. One To One Mapping - Setup Spring Boot Project

--- src/main/java/com/iffat/springchadhibernatejpa/SpringChadHibernateJpaApplication.java
+++ src/main/java/com/iffat/springchadhibernatejpa/SpringChadHibernateJpaApplication.java
@@ -1,7 +1,9 @@
 package com.iffat.springchadhibernatejpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringChadHibernateJpaApplication {
@@ -10,4 +12,10 @@ public class SpringChadHibernateJpaApplication {
         SpringApplication.run(SpringChadHibernateJpaApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return runner -> {
+            System.out.println("Hello world");
+        };
+    }
 }
