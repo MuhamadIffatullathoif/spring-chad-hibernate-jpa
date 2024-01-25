528dbf1b10815af17039a63e22be6c774ae04698
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-25 08:07:11 +0700

    13. One To Many Mapping Bidirectional - Annotate Course Entity

--- src/main/java/com/iffat/springchadhibernatejpa/entity/Course.java
+++ src/main/java/com/iffat/springchadhibernatejpa/entity/Course.java
@@ -1,13 +1,21 @@
 package com.iffat.springchadhibernatejpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "course")
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "title")
     private String title;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
     private Instructor instructor;
 
     public Course() {
